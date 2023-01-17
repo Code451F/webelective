ee3d006f2d7a2c72caf6fa1b0ed2631347ed44f7
Code451F <451fsolo@gmail.com>
2023-01-17 09:13:49 +0530

    mod1 and mod2

---
+++ script.js
@@ -0,0 +1,5 @@
+document.writeln("<h1>Welcome to JavaScript Programming</h1>");
+alert("hello");
+
+document.writeln("<h1 class='jcs'>Welcome to html, css and javascript</h1>");
+window.alert("Welcome to \nJavaScript\nProgramming");
